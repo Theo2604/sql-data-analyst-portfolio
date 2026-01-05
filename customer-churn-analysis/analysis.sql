3c40f01c2ebbc01de886b99837d28b2113d65f5d
Theo Nguyen <trungnguyen260497@gmail.com>
2026-01-05 09:51:51 +0100

    Update analysis.sql

--- customer-churn-analysis/analysis.sql
+++ customer-churn-analysis/analysis.sql
@@ -1,1 +1,15 @@
+WITH last_transaction AS (
+    SELECT
+        customer_id,
+        MAX(transaction_date) AS last_txn_date
+    FROM transactions
+    GROUP BY customer_id
+)
+SELECT
+    c.customer_id,
+    c.signup_date,
+    lt.last_txn_date
+FROM customers c
+LEFT JOIN last_transaction lt
+ON c.customer_id = lt.customer_id;
 
